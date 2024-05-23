4e6662b31e7061e5697fbe272d3863031bf57ba2
Arturo <artuloca@myuax.co>
2024-05-23 17:42:45 +0200

    cambios indice

--- src/app/Inicio.js
+++ src/app/Inicio.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/inicio') // Reemplaza esto con la URL de tu API
+            .then(response => response.json())
+            .then(data => setData(data));
+    }, []);
+
     return (
         <div>
-            <h1>Bienvenido al </h1>
+            <h1>Bienvenido a CodeMasters Academy</h1>
             <p>Aquí puedes gestionar alumnos, profesores y cursos.</p>
+            {data && <p>Datos del servidor: {JSON.stringify(data)}</p>}
         </div>
     );
 }

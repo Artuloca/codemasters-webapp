994106558d3997d31b216d1c9cc5d8f5a1e9f2ad
Arturo <artuloca@myuax.co>
2024-05-23 16:59:09 +0200

    creacion fichas profesor y alumno

--- src/components/FichaProfesor.js
+++ src/components/FichaProfesor.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function FichaAlumno({ nombre, curso }) {
+function FichaProfesor({ nombre, asignatura }) {
     return (
         <div style={{ border: '1px solid black', margin: '10px', padding: '10px' }}>
             <h2>{nombre}</h2>
-            <h3>{curso}</h3>
+            <h3>{asignatura}</h3>
         </div>
     );
 }
 
-export default FichaAlumno;
+export default FichaProfesor;

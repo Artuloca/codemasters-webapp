32c444f05a443ef5e7148130c5e7b35d66e8363f
Arturo <artuloca@myuax.co>
2024-05-23 17:00:04 +0200

    alumnos

---
+++ src/app/Alumnos.js
@@ -0,0 +1,27 @@
+// src/app/alumnos.js
+import React, { useState, useEffect } from 'react';
+import FichaAlumno from '../components/FichaAlumno';
+
+function App() {
+    const [alumnos, setAlumnos] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/alumnos') // Reemplaza esto con la URL de tu API
+            .then(response => response.json())
+            .then(data => setAlumnos(data));
+    }, []);
+
+    return (
+        <div>
+            {alumnos.map((alumno, index) => (
+                <FichaAlumno
+                    key={index}
+                    nombre={alumno.nombre}
+                    curso={alumno.curso}
+                />
+            ))}
+        </div>
+    );
+}
+
+export default App;
